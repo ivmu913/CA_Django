1127a9ade33b3a4b5da311ccc324d191c4cca697
ivmu913 <ivmu913@gmail.com>
2023-04-25 22:00:16 +0300

    added templates and static folders

--- library/views.py
+++ library/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Genre, Author, Book, BookInstance
 
 
-# Create your views here.
 def index(request):
-    return HttpResponse('Labas')
+    book_count = Book.objects.all().count()
+    book_instance_count = BookInstance.objects.all().count()
+    author_count = Author.objects.all().count()
+    available_books_count = BookInstance.objects.filter(book_status__exact='a').count()
+
+    context = {
+        'books': book_count,
+        'book_instances': book_instance_count,
+        'authors': author_count,
+        'available': available_books_count
+    }
+
+    return render(request, "index.html", context)
